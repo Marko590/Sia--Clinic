9eea7bf2b0346b6d22d013f580c14e1bc03f372e
marko590 <marko.bojkovic00@outlook.com>
2025-02-07 19:41:20 +0100

    Moved map to a separate component.

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -2,13 +2,14 @@ import { Box, Paper, Typography } from "@mui/material";
 import React from "react";
 import ContactInfo from "../components/ContactInfo";
 import { useTranslations } from "next-intl";
+import MapPaper from "../components/MapPaper";
 
 const Contact = () => {
   const t = useTranslations("ContactPage");
   return (
     <Box
       sx={{
-        mt: 10,
+        mt: 4,
         width: "100%",
         height: "100%",
         display: "flex",
@@ -58,8 +59,10 @@ const Contact = () => {
         sx={{
           display: "flex",
           flexDirection: { xs: "column", md: "row" },
+          alignItems: "stretch",
+          justifyContent: "center",
           p: 4,
-          width: { xs: "95%", md: "90%" },
+          width: "100%",
           gap: 4,
         }}
       >
@@ -69,54 +72,41 @@ const Contact = () => {
             background: "background.paper",
             width: "100%",
             flex: 1,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "start",
-            flexDirection: "column",
-            gap: 4,
-            p: 4,
           }}
         >
-          <Typography
-            variant="h1"
-            color="primary"
-            sx={{ textAlign: "center", fontSize: "2.5rem", fontWeight: "300" }}
-          >
-            {t("appointmentTitle")}
-          </Typography>
-
-          <ContactInfo
-            title="Working hours"
-            subtitle={[t("workHours"), t("emergencyWorkHours")]}
-          />
-          <ContactInfo title={t("phoneTitle")} subtitle={t("phoneNumber")} />
-          <ContactInfo title={t("addressTitle")} subtitle={t("address")} />
-          <ContactInfo title={t("emailTitle")} subtitle={t("email")} />
-        </Paper>
-
-        <Paper
-          elevation={10}
-          sx={{
-            flex: 1,
-            width: "100%",
-            display: "flex",
-            alignItems: "center",
-            p: 2,
-          }}
-        >
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2832.562834448891!2d20.476941200000002!3d44.76932970000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a710a9139926d%3A0x16c546adf2689e58!2sSIA%20DENTAL%20CLINIC!5e0!3m2!1ssr!2srs!4v1738200939985!5m2!1ssr!2srs"
-            style={{
-              border: 0,
-              width: "100%",
-              height: "100%",
-              borderRadius: "10px",
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "start",
+              flexDirection: "column",
+              p: 2,
+              gap: 4,
             }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          >
+            <Typography
+              variant="h1"
+              color="primary"
+              sx={{
+                textAlign: "center",
+                fontSize: "2.5rem",
+                fontWeight: "300",
+              }}
+            >
+              {t("appointmentTitle")}
+            </Typography>
+
+            <ContactInfo
+              title="Working hours"
+              subtitle={[t("workHours"), t("emergencyWorkHours")]}
+            />
+            <ContactInfo title={t("phoneTitle")} subtitle={t("phoneNumber")} />
+            <ContactInfo title={t("addressTitle")} subtitle={t("address")} />
+            <ContactInfo title={t("emailTitle")} subtitle={t("email")} />
+          </Box>
         </Paper>
+
+        <MapPaper sx={{ flex: 1, minHeight: 560 }} />
       </Box>
     </Box>
   );
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import { Call, LocationOn } from "@mui/icons-material";
 import ServicesGrid from "./components/ServicesGrid";
 import React, { useEffect, useRef, useState } from "react";
 import { useTranslations } from "next-intl";
+import MapPaper from "./components/MapPaper";
 
 export default function Home() {
   const targetBoxRef = useRef<HTMLDivElement | null>(null);
@@ -160,6 +161,7 @@ export default function Home() {
         <ServicesGrid />
       </Box>
       <Box
+        ref={targetBoxRef}
         sx={{
           position: "relative",
           width: "200px",
@@ -194,31 +196,7 @@ export default function Home() {
           {t("locationTitle")}
         </Typography>
       </Box>
-      <Paper
-        ref={targetBoxRef}
-        sx={{
-          m: 8,
-          background: "background.paper",
-          width: { xs: "90%" },
-          height: { xs: 600, md: 800 },
-          display: "flex",
-          alignItems: "center",
-          p: 2,
-        }}
-      >
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2832.562834448891!2d20.476941200000002!3d44.76932970000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a710a9139926d%3A0x16c546adf2689e58!2sSIA%20DENTAL%20CLINIC!5e0!3m2!1ssr!2srs!4v1738200939985!5m2!1ssr!2srs"
-          style={{
-            border: 0,
-            width: "100%",
-            height: "100%",
-            borderRadius: "10px",
-          }}
-          allowFullScreen
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </Paper>
+      <MapPaper sx={{ width: "90%", height: { xs: 600, md: 800 } }} />
     </Box>
   );
 }
