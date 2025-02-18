5535506eafba1d9669c2befd9cb492fa385f935c
Marko590 <marko.bojkovic00@outlook.com>
2025-02-18 17:06:06 +0100

    Added metadata from translations to all static routes.

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -3,8 +3,38 @@ import React from "react";
 import HoverCard from "../components/HoverCard";
 import { servicesKeys } from "../lib/data";
 import SectionTitle from "../components/SectionTitle";
+import { getTranslations } from "next-intl/server";
+import { Metadata } from "next";
 
-const page = () => {
+type Props = {
+  params: { locale: string };
+};
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale } = params;
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return {
+    title: t("services.title"),
+    description: t("services.description"),
+    keywords: t("services.keywords"),
+    openGraph: {
+      title: t("services.openGraph.title"),
+      description: t("services.openGraph.description"),
+      type: "website",
+      url: t("services.openGraph.url"),
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@siadentalclinic",
+      creator: "@siadentalclinic",
+      title: t("services.twitter.title"),
+      description: t("services.twitter.description"),
+    },
+    robots: "index, follow",
+  };
+}
+
+const Page = () => {
   return (
     <>
       <SectionTitle title={"Services"} sx={{ mt: 4 }} />
@@ -25,4 +55,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
